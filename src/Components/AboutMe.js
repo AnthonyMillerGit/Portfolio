8505885e906fcc37b37d14ff4b3a2f7c5cffa4a6
Anthony Miller <anthony@amiller.info>
2024-03-06 09:29:01 -0700

    add skills to aboutme

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FaCode, FaLaptopCode } from 'react-icons/fa';
 import { SectionContainer, SectionTitle, SectionText } from '../Components/Styles/SharedStyles';
 import profilePic from '../Media/Didier.jpeg';
 
@@ -8,7 +7,7 @@ const EntireContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-around; // Adjust spacing around items
+  justify-content: space-around;
   width: 100%;
 `;
 
@@ -17,62 +16,70 @@ const ProfilePicture = styled.img`
   width: 200px;
   height: 200px;
   object-fit: cover;
-  margin: 30px 0; // Increase space above and below the image
+  margin: 30px 0;
 `;
 
 const AboutMeContent = styled.div`
   display: flex;
-  flex-direction: column; // Change to column for aligning text under the image
-  align-items: center; // Center-align the text content
-  text-align: center; // Ensure text is centered
-  width: 80%; // Adjust width as necessary
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  width: 80%;
 `;
 
 const SkillsContainer = styled.div`
   display: flex;
-  justify-content: center;
-  gap: 40px;
-  flex-wrap: wrap;
-  margin-top: 20px; // Add some space between the text and skills
+  flex-direction: column; // Changed to column to align title above skills
+  align-items: center; // Center align the contents
+  gap: 20px;
+  margin-top: 20px;
 `;
 
 const Skill = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 10px;
-  flex-basis: 20%;
+  flex-direction: row; // Align skills in a row for a horizontal layout
+  flex-wrap: wrap; // Allow skills to wrap
+  justify-content: center; // Center the skills
+  width: 100%; // Use full width to center content
 `;
 
-const SkillIcon = styled.div`
-  font-size: 3rem;
-  color: #14FFEC;
-  &:hover {
-    color: #0D7377;
-    transform: scale(1.1);
-  }
-  transition: color 0.3s ease, transform 0.3s ease;
-`;
+const skills = [
+  'JavaScript',
+  'React',
+  'HTML',
+  'CSS',
+  'Python',
+  'Java',
+  'Golang',
+  'SQL',
+  'PostgreSQL',
+  'MongoDB',
+  'Node.js',
+  'Express',
+  'Git',
+  'Docker',
+  'Kubernetes',
+  'AWS',
+  'Azure',
+  'Terraform',
+  'Puppet/Ansible'
+];
+
+const skillsString = skills.join(', ');
 const AboutMe = () => (
+
   <SectionContainer>
     <EntireContainer>
       <ProfilePicture src={profilePic} alt="Your Name" />
       <AboutMeContent>
-        <SectionTitle>About Me</SectionTitle>
+        <SectionTitle>Welcome</SectionTitle>
         <SectionText>
-          Here's a brief overview of who I am, my journey, and what I love doing. 
-          Dive into the details of my projects below to see the work I'm most proud of.
+          Hello, my name is Anthony, and I am a software engineer based out of Phoenix, Arizona. With a passion for creating and learning, my journey in technology has been driven by curiosity and the desire to solve complex problems. My expertise spans developing innovative applications to optimizing user experiences, reflecting my commitment to enhancing the way we interact with digital environments. Outside of coding, I thrive on exploring new technologies and continuously expanding my skill set to stay at the forefront of industry trends. My goal is to craft solutions that not only meet immediate needs but also anticipate future challenges.
         </SectionText>
       </AboutMeContent>
       <SkillsContainer>
-        <Skill>
-          <SkillIcon><FaCode /></SkillIcon>
-          <SectionText>Development</SectionText>
-        </Skill>
-        <Skill>
-          <SkillIcon><FaLaptopCode /></SkillIcon>
-          <SectionText>Design</SectionText>
-        </Skill>
+        <SectionTitle>Some of my areas of expertise include:</SectionTitle>
+        <SectionText>{skillsString}</SectionText>
       </SkillsContainer>
     </EntireContainer>
   </SectionContainer>
